3fc2b9fa47d62561ada77f2ace8c4f779ccca7c0
ben <benkenawell@pm.me>
2024-03-24 21:24:08 -0400

    switch out dayjs for luxon to improve the date math

--- app/javascript/controllers/timer_controller.js
+++ app/javascript/controllers/timer_controller.js
@@ -1,9 +1,5 @@
 import { Controller } from "@hotwired/stimulus"
-import dayjs from 'dayjs';
-import relativeTime from 'dayjs/plugin/relativeTime';
-import duration from 'dayjs/plugin/duration';
-dayjs.extend(relativeTime);
-dayjs.extend(duration);
+import { DateTime } from 'luxon';
 
 // Connects to data-controller="timer"
 export default class extends Controller {
@@ -12,36 +8,32 @@ export default class extends Controller {
   
   connect() {
     this.interval = setInterval(this.updateTime.bind(this), 500);
-    console.log(
-      'date',
-      dayjs(this.dateValue).fromNow(),
-      this.interval,
-      this.element
-    );
     this.updateTime();
   }
 
   disconnect() {
-    console.log('disconnecting');
     clearInterval(this.interval);
   }
 
+
   updateTime () {
-    const duration = dayjs.duration(dayjs(this.dateValue).diff(dayjs()));
+    const now = DateTime.now();
+    const targetDate = DateTime.fromISO(this.dateValue);
+    const duration = targetDate.diff(now, ['month', 'day', 'hour', 'minute', 'second', 'millisecond']);
     
-    const month = duration.months();
+    const month = duration.months;
     this.monthTarget.innerHTML = `${month} month${month === 0 ? '' : 's'}`;
     
-    const day = duration.days();
-    this.dayTarget.innerHTML = `${duration.format('D')} day${day === 1 ? '' : 's'}`;
+    const day = duration.days;
+    this.dayTarget.innerHTML = `${day} day${day === 1 ? '' : 's'}`;
     
-    const hour = duration.hours();
+    const hour = duration.hours;
     this.hourTarget.innerHTML = `${hour} hour${hour === 1 ? '' : 's'}`;
     
-    const minute = duration.minutes();
+    const minute = duration.minutes;
     this.minuteTarget.innerHTML = `${minute} minute${minute === 1 ? '' : 's'}`;
 
-    const second = duration.seconds();
+    const second = duration.seconds;
     this.secondTarget.innerHTML = `${second} second${second === 1 ? '' : 's'}`;
   }
 }
